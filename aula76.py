490bdf7ad42843637c84b6defe850249795986f0
L-coutinho <leandrocoutinho.tucano@gmail.com>
2023-07-04 15:54:49 -0300

    Aula 77

--- aula76.py
+++ aula76.py
@@ -9,25 +9,143 @@
 # pop - Apaga um item com a chave especificada (del)
 # popitem - Apaga o último item adicionado
 # update - Atualiza um dicionário com outro
+
+
+# pessoa = dict(nome="Leandro", sobrenome="Coutinho")
+
+# pessoa = {
+#     'nome': 'Leandro',
+#     'sobrenome': 'Coutinho',
+#     'idade': 23,
+#     'altura': '1.65',
+#     'endereços': [
+#         {'rua': 'tal tal', 'número': 123},
+#         {'rua': 'outra rua', 'número': 321},
+#     ],
+# }
+
+# print(pessoa['nome'])
+
+# for chave in pessoa:
+#     print(chave, pessoa[chave])
+
+# # Apagando chaves
+
+# pessoa['carro'] = 'HB20'
+# print(pessoa)
+
+# del pessoa['carro']
+# print(pessoa)
+
+
+# # Tentando acessar chave
+
+# if pessoa.get('carro') is None:
+#     print('Ñ existe')
+# else:
+#     print('Existe')
+
+# pessoa = {
+#     'nome': 'Leandro',
+#     'sobrenome': 'Coutinho',
+#     'idade': 900
+# }
+
+# print(pessoa.__len__())
+# print(len(pessoa))
+# print(list(pessoa.keys()))
+# print(list(pessoa.values()))
+# print(list(pessoa.items()))
+
+
+# for valor in pessoa.values():
+#     print(valor)
+
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
+# pessoa.setdefault('idade', 0)
+# print(pessoa['idade'])
+
+# Apontam para o mesmo local na memória
+# d1 = {
+#     'c1': 1,
+#     'c2': 2,
+# }
+# d2 = d1
+# d2['c1'] = 1000
+# print(d1)
+
+
+# Shallow copy (Só copia valores imutáveis, se for uma lista ele aponta para o mesmo local)
+
+# d1 = {
+#     'c1': 1,
+#     'c2': 2,
+#     'l1': [0, 1, 2],
+# }
+
+# d2 = d1.copy()
+
+# d2['c1'] = 1000
+# d2['l1'][1] = 9999
+# print(d1)
+# print(d2)
+
+# Deep copy
+# import copy
+
+
+# d1 = {
+#     'c1': 1,
+#     'c2': 2,
+#     'l1': [0, 1, 2],
+# }
+
+# d2 = copy.deepcopy(d1)
+
+# d2['c1'] = 1000
+# d2['l1'][1] = 9999
+# print(d1)
+# print(d2)
+
+# Get
+
 p1 = {
-    'nome': 'Luiz',
-    'sobrenome': 'Miranda',
+    'nome': 'Leandro',
+    'sobrenome': 'Coutinho',
 }
-# print(p1['nome'])
+
 # print(p1.get('nome', 'Não existe'))
+
+# Pop
 
 # nome = p1.pop('nome')
 # print(nome)
 # print(p1)
+
+# Popitem (Exclui a última chave)
+
 # ultima_chave = p1.popitem()
 # print(ultima_chave)
 # print(p1)
+
+
+# Update
 # p1.update({
-#     'nome': 'novo valor',
-#     'idade': 30,
+#     'nome':'novo valor',
+#     'idade': 23,
 # })
+
+# print(p1)
+
+# ou 
+
 # p1.update(nome='novo valor', idade=30)
-# tupla = (('nome', 'novo valor'), ('idade', 30))
-lista = [['nome', 'novo valor'], ['idade', 30]]
-p1.update(lista)
+# print(p1)
+
+# ou
+tupla = ('nome', 'novo valor'), ('idade', 30)
+p1.update(tupla)
 print(p1)
--- aula77.py
+++ aula77.py
@@ -18,40 +18,24 @@ perguntas = [
         'Resposta': '5',
     },
 ]
+acertos = 0
 
-qtd_acertos = 0
-for pergunta in perguntas:
-    print('Pergunta:', pergunta['Pergunta'])
-    print()
+for dicionario in perguntas:
+    print(f"\nPergunta: {dicionario['Pergunta']}")
+    print("Opções:")
+    for i, opcao in enumerate(dicionario['Opções']):
+        print(f"{i}) {opcao}")
+        
+    resp = input('Escolha uma opção: ')
+    try:
+        resp = int(resp)
+        if dicionario['Opções'][resp] == dicionario['Resposta']:
+            acertos += 1
+            print("Acertou 👍")
+        else:
+            print("Errou ❌")
+    except ValueError:
+        print("Errou ❌")
 
-    opcoes = pergunta['Opções']
-    for i, opcao in enumerate(opcoes):
-        print(f'{i})', opcao)
-    print()
-
-    escolha = input('Escolha uma opção: ')
-
-    acertou = False
-    escolha_int = None
-    qtd_opcoes = len(opcoes)
-
-    if escolha.isdigit():
-        escolha_int = int(escolha)
-
-    if escolha_int is not None:
-        if escolha_int >= 0 and escolha_int < qtd_opcoes:
-            if opcoes[escolha_int] == pergunta['Resposta']:
-                acertou = True
-
-    print()
-    if acertou:
-        qtd_acertos += 1
-        print('Acertou 👍')
-    else:
-        print('Errou ❌')
-
-    print()
-
-
-print('Você acertou', qtd_acertos)
-print('de', len(perguntas), 'perguntas.')
+print(f"\nVocê acertou {acertos}"
+        f" de {len(perguntas)} perguntas.")
